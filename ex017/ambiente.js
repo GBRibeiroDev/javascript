51e761c70687f139887e42e35a899a6fb21f67d2
GabriellConstantino <gabribeirodev@gmail.com>
2025-01-13 21:08:39 -0300

    atualizei ex017

--- ex017/ambiente.js
+++ ex017/ambiente.js
@@ -1,11 +1,16 @@
 let c = 1
-while (c < 6) {
-    console.log('Tudo bem?')
-    c++
+do {
+console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 7)
+
+/*
+let c = 1
+while (c <= 3) {
+console.log(`Passo ${c}`)
+    c++ // c = c + 1
 }
-
-
-
+*/
 
 
 /*
