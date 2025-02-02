80f43758f1f615e712635b0bebe3ffd9dc06d966
GabriellConstantino <gabribeirodev@gmail.com>
2025-02-02 19:58:25 -0300

    criei ex sobre functions.

---
+++ ex022/ambiente.js
@@ -0,0 +1,17 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimpar(6))
+
+// Podemos fazer colocando a ação da variavel e o parametro direto no console.log ou da maneira abaixo.
+
+// Podemos fazer da maneira em que adicionamos o nosso paramêtro dentro de uma varíavel igual o exemplo abaixo. 
+/*
+let res = parimpar(6)
+console.log(res)]
+*/ 
